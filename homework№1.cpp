48930f3f06c4e1e3e0726e370fef8240159d539b
xenia <kseniakotyga@gmail.com>
2023-07-29 18:46:46 +0300

    Inizial commit

---
+++ homework№1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <fstream>
+
+
+int main(int argc, char** argv) {
+
+    std::fstream file (" in.txt ");
+
+    file.open(" in.txt ");
+
+    if ( file.is_open() ) {
+        std::cout << " OPEN ";
+    }
+    else {
+        std::cout << " CLOSE ";
+    }
+
+    file.close();
+
+    return 0;
+}
+
